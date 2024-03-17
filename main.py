9d7c0f4e52999bd546ad6f1dd7827a5638daaa86
drohankoirala <126786936+drohankoirala@users.noreply.github.com>
2024-03-17 14:22:16 +0545

    Add files via upload

---
+++ main.py
@@ -0,0 +1,340 @@
+import os
+
+if not os.path.exists('res\\Music-data.json'):
+    import setup
+    setup.from_main = True
+    setup.Setup()
+    setup.runShell()
+    exit()
+
+import datetime
+import json
+import random
+import time
+from flask import Flask, jsonify, request, abort, send_from_directory, send_file, make_response, render_template
+
+app = Flask(__name__)
+dataCopy = None
+list_ = []
+
+
+class SHandler:
+    def __init__(self):
+        with open('res\\Music-data.json') as d:
+            self.data = json.load(d)
+            if len([_ for _ in self.data.keys()]) <= 3:
+                import setup
+                setup.from_main = True
+                setup.Setup()
+                setup.runShell()
+                exit()
+        self.this = str(datetime.datetime.now().strftime("%Y%m%d%H"))
+        if not self.data.get("act"):
+            self.data['act'] = {}
+        self.data["act"][self.this] = self.data.get('act', {}).get(self.this) or {}
+        self.ine = {}
+        self.recommend_ = None
+        self.arrange = None
+
+    @staticmethod
+    def checkValidation(key: str):
+        if key:
+            try:
+                curMilliSec = int(time.time())
+                argMilliSec = int(key, 24) / 1000
+                if (curMilliSec - 5) < argMilliSec < (curMilliSec + 5):
+                    return True
+            except Exception:
+                pass
+        return False
+
+    def saveMusic(self):
+        with open('res\\Music-data.json', 'w') as df:
+            json.dump(self.data, df)
+
+    def rec_(self):
+        rData = {}
+        for kl in self.data:
+            if type(self.data[kl]) is dict:
+                cvb = self.data[kl].get('act')
+                if cvb and not rData.get(kl):
+                    rData[kl] = (cvb.get('dur') or 0) * (cvb.get('count') or 0)
+
+        self.arrange = [song_id for song_id, _ in sorted(rData.items(), key=lambda x: x[1], reverse=True)]
+        return self.arrange
+
+    def recommend_songs(self, force=False, options=None):
+        if options is None:
+            options = {}
+        if self.recommend_ and not force:
+            return self.recommend_
+        id_ = options.get('user', datetime.datetime.now().strftime("%Y%m%d%h"))
+        if self.ine.get(id_):
+            print('returned')
+            return self.ine[id_]
+        user_data = self.data.get("act", {})
+        today = user_data.get(id_)
+        if not today:
+            self.recommend_ = []
+            return self.recommend_
+        filtered_data = {
+            user: {song: duration for song, duration in songs.items() if duration > 20}
+            for user, songs in user_data.items()
+        }
+        our_user = set(today.keys())
+        most_common = [
+            [user, len(set(data_.keys()) & our_user)] for user, data_ in filtered_data.items() if user != today
+        ]
+        most_common.sort(key=lambda x: x[1], reverse=True)
+
+        found_songs = {}
+        for user, _ in most_common[:30]:
+            for song, duration in filtered_data[user].items():
+                if song not in our_user and song not in found_songs:
+                    found_songs[song] = duration
+
+        self.recommend_ = [song_id for song_id, _ in sorted(found_songs.items(), key=lambda x: x[1], reverse=True)]
+        self.ine[id_] = self.recommend_
+        return self.recommend_
+
+
+ghn = SHandler()
+ghn.rec_()
+
+
+def respo(code=404, file='400.html'):
+    response = make_response(render_template(file))
+    response.status_code = code
+    return response
+
+
+@app.errorhandler(400)
+def notfound(n):
+    return respo(400, '400.html')
+
+
+@app.errorhandler(404)
+def notfound(n):
+    return respo(404, '400.html')
+
+
+# @app.route('/api/recommended')
+# def recommended():
+#     rData = ghn.arrange or ghn.rec_()
+#     return jsonify(rData[:30]), 200
+
+
+@app.route('/thumb')
+def thumbnail():
+    loc = request.args.get('id')
+    if not loc:
+        abort(400)
+    img = ghn.data[loc]['src']['img']['limg'].split('\\')[-1]
+    return send_from_directory("Music\\thumbnails\\", img)
+
+
+@app.route('/api/lists')
+def list__():
+    id_ = request.args.get('id')
+    if not id_:
+        abort(400)
+    elif id_ == '_':
+        rData = ghn.data.get('lists', {})
+    else:
+        rData = ghn.data.get('lists', {}).get(id_, {})
+    return jsonify(rData), 200
+
+
+@app.route('/api/views')
+def views():
+    sKeys = set()
+    try:
+        sKeys.add(ghn.arrange[0])
+        rData = {"ls": [{
+            "ur": 'songs?view_id=recommended&play=' + ghn.arrange[0],
+            'tl': ghn.data[ghn.arrange[0]]['t']['c']}],
+            'vd_top': {
+                'ls': []
+            },
+            'vd_lss': {
+                'ls': []
+            },
+            'vd_mid': {
+                'ls': []
+            }}
+    except IndexError:
+        rData = {"ls": [{
+            "ur": None,
+            'tl': None, }],
+            'vd_top': {
+                'ls': []
+            },
+            'vd_lss': {
+                'ls': []
+            },
+            'vd_mid': {
+                'ls': []
+            }}
+
+    trf_ = [kjh for kjh in ghn.data.get('act', [])]
+    random.shuffle(trf_)
+    sKeys = []
+    sKeys_ = [ghn.arrange[0]]
+    for ind, ijh in enumerate(trf_):
+        rfg = ghn.recommend_songs(True, {'user': ijh})
+        if not rfg:
+            continue
+        id_ = rfg[ind]
+        for i in range(1, len(rfg)):
+            if id_ in sKeys_ and sKeys_:
+                id_ = rfg[i]
+                continue
+            sKeys_.append(id_)
+            break
+        rfg = ghn.data[id_]
+        rData['ls'].append({
+            "ur": 'songs?view_id=' + ijh + '&play=' + id_,
+            'tl': rfg['t']['c'], })
+        sKeys.append(ijh)
+
+    ghn.ine["recommended"] = sKeys
+    sKeys = []
+    iuj = ghn.arrange[:50]
+    random.shuffle(iuj)
+    for jhn in iuj:
+        if len(sKeys) >= 12:
+            break
+        elif jhn in sKeys:
+            continue
+        cur = ghn.data[jhn]
+        rData['vd_top']['ls'].append({'ig': cur['src']['img']['limg'],
+                                      "ur": jhn,
+                                      'tl': cur['t']['c']})
+        sKeys.append(jhn)
+    rData['vd_top']['lst'] = sKeys
+    sKeys = []
+    iuj = ghn.arrange[-50:]
+    random.shuffle(iuj)
+    for jhn in iuj:
+        if len(sKeys) >= 12:
+            break
+        elif jhn in sKeys:
+            continue
+        cur = ghn.data[jhn]
+        rData['vd_lss']['ls'].append({'ig': cur['src']['img']['limg'],
+                                      "ur": jhn,
+                                      'tl': cur['t']['c']})
+        sKeys.append(jhn)
+    rData['vd_lss']['lst'] = sKeys
+    sKeys = []
+    iuj = ghn.arrange[50:-50]
+    random.shuffle(iuj)
+    for jhn in iuj:
+        if len(sKeys) >= 24:
+            break
+        elif jhn in sKeys:
+            continue
+        cur = ghn.data[jhn]
+        rData['vd_mid']['ls'].append({'ig': cur['src']['img']['limg'],
+                                      "ur": jhn,
+                                      'tl': cur['t']['c']})
+        sKeys.append(jhn)
+    rData['vd_mid']['lst'] = sKeys
+
+    return jsonify(rData), 200
+
+
+@app.route('/api/view')
+def view():
+    hgf = request.args.get('view_id')
+    if not hgf:
+        abort(400)
+    rData = ghn.recommend_songs(True, options={'user': hgf})
+    for jnb in ghn.ine:
+        print(ghn.ine[jnb], 's')
+    return jsonify(rData), 200
+
+
+@app.route('/api/get_songs')
+def get_songs():
+    global dataCopy, list_
+    if not dataCopy:
+        dataCopy = dataCopy or {}
+        tTemp = ghn.data.copy()
+        for jk in tTemp:
+            if jk not in ['loc', 'act', 'loc_song', 'lists']:
+                dataCopy[jk] = tTemp[jk]
+                list_.append(jk)
+
+    songs = {"files": dataCopy, 'fetched': time.time(), 'playlist': list_}
+    return jsonify(songs)
+
+
+@app.route("/api/opt", methods=["POST"])
+def add_opt():
+    jk = request.json.get('option')
+    typeOFRequest = jk.get('type')
+    if not jk.get('id'):
+        abort(400)
+    stem = ghn.data[jk.get('id')]
+    ui = (stem.get('act')) or {'count': 0, 'd': [], 'dur': 0}
+    if typeOFRequest == 'change':
+        ui['d'].append(jk['cdur'])
+
+    elif typeOFRequest == 'interval':
+        fvb = (jk.get('Pdur') / stem['src']['len'])
+        fvb = 100 if fvb > 1 else (fvb * 100)
+        ui['dur'] = (ui.get('dur') or 0) + fvb
+        ghn.data['act'][ghn.this][jk.get('id')] = fvb + ghn.data['act'][ghn.this].get(jk.get('id'), 0)
+
+    elif typeOFRequest == 'click':
+        ui['count'] += 1
+        ui['dur'] += 10
+
+    elif typeOFRequest == 'item_skip':
+        ui['count'] -= (1 if ui.get('count', 0) > 5 else 0)
+        ui['dur'] -= (5 * (stem['src']['len'] / 100)) if ui.get('dur', 0) > 20 else 0
+
+    elif typeOFRequest == 'song-ended':
+        ui['dur'] += (5 * (stem['src']['len'] / 100))
+
+    ghn.data[jk['id']]['act'] = ui
+    ghn.saveMusic()
+    return '', 200
+
+
+@app.route("/api/audio")
+def stream_audio():
+    file = request.args.get('id')
+    if not file:
+        abort(400)
+    file = ghn.data[file]['src']['lsrc'].split('\\')[-1]
+    return send_from_directory("Music\\", file)
+
+
+@app.route("/")
+def player():
+    return send_file("templates/index.html")
+
+
+@app.after_request
+def add_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Origin, Accept'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST'
+    return response
+
+
+@app.before_request
+def check():
+    if '/api' not in request.path:
+        return
+    key = request.args.get('key')
+    if not ghn.checkValidation(key or ''):
+        abort(400)
+
+
+if __name__ == '__main__':
+    # import webbrowser; webbrowser.open('http://localhost:5000/')
+    print('---- Listening on "http://localhost:5000/" Enjoy! ----')
+    app.run(host='0.0.0.0', port=5000)
---
+++ setup.py
@@ -0,0 +1,285 @@
+import datetime
+import json
+import os
+import shutil
+import time
+
+import requests
+
+try:
+    from pytube import YouTube, Playlist
+    from selenium.webdriver.common.by import By
+    from selenium import webdriver
+except ImportError:
+    os.system('pip install pytube selenium')
+
+current_time_str = datetime.datetime.now().strftime('%Y-%m-%d')
+ti = datetime.datetime.strptime(current_time_str, '%Y-%m-%d')
+from_main = False
+os.system('cls')
+print("""HNPlayer Handler, Version 1.0.1, by drohan""")
+if not os.path.exists('res\\Music-Data.json'):
+    os.system('mkdir res')
+    with open('res\\Music-Data.json', 'w') as d:
+        print("Created \"res\\Music-Data.json\", Reason [json file was deleted/haven't run"
+              " \"setup.py\" before/json files location was changed]")
+        json.dump({'loc': os.getcwd() + '\\Music\\', 'loc_song': os.getcwd() + '\\Music\\thumbnails\\'}, d)
+
+with open('res\\Music-Data.json') as d:
+    list_ = json.load(d)
+
+if not os.path.exists(list_['loc_song']):
+    os.mkdir(list_['loc_song'])
+
+def Setup():
+    print("""
+Before running "main.py," ensure you have at least one song installed. You can add songs to the system using simple commands:
+
+To download one song: dwd  -f  https://www.youtube.com/watch?v=----VIDEO_ID----
+To download all songs from a playlist: dwd  -l  https://www.youtube.com/playlist?list=----list_ID----
+For more commands, type "help."
+
+This script stores user activity in "Music-data.json" in the "res/" directory. Make sure not to remove this file. The JSON file contains information about songs, you interaction on songs, for recommendation. You can use simple commands to download, remove, change the location, or reinstall mistakenly removed items, along with the thumbnail of the item from YouTube.
+
+All the downloaded item are stored on "Music" directory "./Music", use command to change the location else not
+
+Try the command "help" for more details.
+""")
+
+
+def save():
+    with open('res\\Music-Data.json', 'w') as ds:
+        json.dump(list_, ds)
+
+
+class Download:
+    def clear(self, string):
+        rSt = ''
+        for fg in string:
+            if fg not in ['<', '>', ':', '"', '/', '\\', '|', '?', '*']:
+                rSt += fg
+        return rSt
+
+    def download(self, url, options=None, force=False):
+        if options is None:
+            options = {}
+        fvc = list_.get(url.split('v=')[1])
+        print(f'Download "{url}"', end='')
+        if fvc and not force:
+            if os.path.exists(fvc['src']['lsrc']):
+                print(' [Skipped]')
+                return
+        try:
+            rfc = YouTube(url)
+            t_l = self.clear(rfc.title)
+            rfc.streams.filter(only_audio=True).first().download(list_['loc'], filename_prefix=t_l, filename='.mp3')
+        except Exception as e:
+            print(f' [Failed] [Reason => {e}]')
+            return
+        img_ = requests.get(rfc.thumbnail_url).content
+        lim_ = f"{list_['loc_song']}{t_l}.jpg"
+        with open(lim_, 'wb') as _:
+            _.write(img_)
+        act_ = (list_.get(rfc.video_id) or {})
+        title = options.get('title') or (act_.get('t') or {}).get('c') or rfc.title
+        act_ = act_.get('act') or {"count": 0, "d": []}
+        list_[rfc.video_id] = {
+            'src': {
+                'csrc': rfc.watch_url,
+                'lsrc': list_['loc'] + t_l + '.mp3',
+                'img': {
+                    'cimg': rfc.thumbnail_url,
+                    'limg': lim_
+                },
+                'len': rfc.length
+            },
+            't': {
+                'c': title,
+                'l': t_l,
+            },
+            "act": {
+                "count": act_.get("count") + 1,
+                "d": act_.get('d'),
+                "dur": (act_.get("dur") or 0) + 50
+            }
+        }
+        save()
+        print(" [Done]")
+
+
+def runShell():
+    while True:
+        multiArgs = input(f"\n{ti} > ").split('  ')
+
+        def get_argument(index):
+            try:
+                return multiArgs[index]
+            except IndexError:
+                return None
+
+        cmd = get_argument(0)
+
+
+
+        if cmd == 'move':
+            target_dir = get_argument(1)
+            if not target_dir or not os.path.exists(target_dir) or (target_dir[-1] != '/' and target_dir[-1] != '\\'):
+                print('Error: (Invalid Path/Not ended with backslash) for move --move-dir--')
+                continue
+            skp = 0
+            if not os.path.exists(target_dir + 'thumbnails/'):
+                os.mkdir(target_dir + 'thumbnails/')
+            move_song_only = get_argument(2) == '--song'
+            move_img_only = get_argument(2) == '--img'
+            for kl in list_:
+                if type(list_[kl]) is dict:
+                    if type(list_[kl].get('src')) is not dict:
+                        continue
+                    cur = list_[kl]['src']['lsrc']
+                    cur_song = list_[kl]['src']['img']['limg']
+                    if not os.path.exists(cur) or not os.path.exists(cur_song):
+                        skp += 1
+                        continue
+                    chg = list_[kl]['src']['lsrc'].replace(list_['loc'], target_dir)
+                    print(list_[kl]['src']['img']['limg'], list_['loc_song'])
+                    chg_song = list_[kl]['src']['img']['limg'].replace(list_['loc_song'], target_dir)
+                    if move_img_only:
+                        print(cur_song, ' => ', chg_song)
+                        shutil.move(cur_song, chg_song)
+                        list_[kl]['src']['img']['limg'] = chg_song
+                    elif move_song_only:
+                        shutil.move(cur, chg)
+                        list_[kl]['src']['lsrc'] = chg
+                    else:
+                        print('Error: Missing parameters.')
+                        continue
+            if move_img_only:
+                list_['loc_song'] = target_dir
+            if skp:
+                print(f'Items were restored to {target_dir}, {skp} items were not found on {list_["loc"]}\nRun "scan" '
+                      f'for details.')
+
+            list_['loc'] = target_dir
+            save()
+
+        elif cmd == 'scan':
+            if get_argument(1) == '-u':
+                continue
+            j = 0
+            miss = []
+            for kl in list_:
+                if type(list_[kl]) is dict:
+                    if type(list_[kl].get('src')) is not dict:
+                        continue
+                    cur = list_[kl]
+                    songEx = os.path.exists(cur['src']['lsrc'])
+                    thumEx = os.path.exists(cur['src']['img'].get('limg') or 'Invalid/path')
+                    if not songEx or not thumEx:
+                        j += 1
+                        miss.append([
+                            f"""[404 ==> {"song" if not songEx else "_img"}] "{cur['t']['c'][:35]}"-  [Failed] for ({cur['src']['lsrc']})""",
+                            kl])
+                    else:
+                        if get_argument(1) != '-r':
+                            print(f"""[200] [Succes] "{cur['t']['c']}"-""")
+
+            for _ in miss:
+                if get_argument(1) == '-r':
+                    del list_[_[1]]
+                print(_[0])
+            if j:
+                print(f"""\nRun "dwd  -m" to reinstall missing songs
+    Run "dwd  -t  -m" to reinstall missing songs img,
+    Run "scan  -r" to remove missing items,
+    {j} items are missing\n""")
+            else:
+                print('\nEverything looks good.\n')
+            save()
+
+        elif cmd == 'dwd':
+            if get_argument(1) == '-m':
+                for kl in list_:
+                    if type(list_[kl]) is dict:
+                        if type(list_[kl].get('src')) is not dict:
+                            continue
+                        Download().download(list_[kl].get('src')['csrc'],
+                                            force=True if get_argument(2) == '-f' else False)
+                print('\nAll missing items were restored.')
+
+            elif get_argument(1) == '-l' and '?list=' in get_argument(2):
+                l = Playlist(get_argument(2))
+                list_['lists'][l.playlist_id] = {"t": l.title, "l": []}
+                for k in l.video_urls:
+                    list_['lists'][l.playlist_id]['l'].append(k.split('v=')[-1])
+                    Download().download(k, force=True if get_argument(3) == '-f' else False)
+                if not l.video_urls:
+                    print('Error: Nothing found (/Private list)')
+                save()
+            elif get_argument(1) == '-f' and 'v=' in get_argument(2):
+                Download().download(get_argument(2), force=True if get_argument(3) == '-f' else False)
+
+            elif get_argument(1) == '-t':
+                if get_argument(2) == '-m':
+                    for kl in list_:
+                        if type(list_[kl]) is dict:
+                            if type(list_[kl].get('src')) is not dict:
+                                continue
+                            cur = list_[kl]['src']['img'].get('limg') or 'invalid/path'
+                            if not os.path.exists(cur):
+                                limg = f"{list_['loc']}thumbnails/{list_[kl]['t']['l']}.jpg"
+                                cimg = list_[kl]['src']['img']['cimg']
+                                print(f'Download "{cimg}"', end='')
+                                img = requests.get(cimg).content
+                                with open(limg, 'wb') as d:
+                                    d.write(img)
+                                list_[kl]['src']['img']['limg'] = limg
+                                print(" [Done]")
+                                save()
+
+            else:
+                print('Error: Missing parameters')
+
+        elif cmd == 'help':
+            print("""HNPlayer Handler, Version 1.0.1, by drohan
+
+curl -url-: [only for me]
+Downloads music files from a specified URL. The URL should contain ?v= and list= parameters.
+Example: curl https://www.youtube.com/watch?v=VIDEO_ID&list=PLAYLIST_ID
+
+move --move-dir--:
+Moves downloaded music files to a specified directory. The directory path should end with a backslash (/).
+Example: move C:/Users/username/Music/
+
+scan:
+Scans for missing music files. It checks if the files listed in the JSON file exist in the current directory.
+
+dwd -m:
+Downloads missing music files. It downloads the files listed in the JSON file that are missing from the current directory.
+
+dwd -l url:
+Downloads music files from a playlist URL. The URL should contain ?v= and list= parameters.
+Example: dwd -l https://www.youtube.com/playlist?list=PLAYLIST_ID
+Note: Your Playlist should be Unlisted or Public.
+
+dwd -f url:
+Downloads a specific music file from a URL. The URL should contain a ?v= parameter.
+Example: dwd -f https://www.youtube.com/watch?v=VIDEO_ID
+
+exit:
+Exits the program.
+
+Note: Don't run any command marked [only for me] and use 2 space for args,
+Eg: dwd -f -url- [use of single space is not allowed].
+: dwd  -f  -url- [use of double space is only allowed].
+""")
+
+        elif cmd == 'exit':
+            break
+
+        else:
+            print(f'Error: (Unknown/Invalid/Single Space separation), Run "help" for details.')
+
+
+if __name__ == '__main__':
+    runShell()
+
